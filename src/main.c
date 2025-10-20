8a323cbd680794879247b80518063475d0781b7c
Pete Skeggs <plskeggs@noeticdesign.com>
2025-10-20 12:37:25 -0700

    Set up initial solar card board files
    Based on mcxn947-protocard.
    
    Note: driver 'ti,tmp101' does not exist yet.
    
    None of the GPIOs for the alert pins or the LT1618 enable
    pin are defined yet.
    
    DAC and I2C use is disabled for now.
    
    Binky and serial logging should work.

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/**
+ * main.c
+ *
+ * Simple main that only logs a bootup message. The remainder
+ * of the demos are implemented as independent threads
+ * in blink.c, dac.c, i2c_sensor.c, and adc.c.
+ *
+ * These can be disabled at compile time by adding:
+ *   CONFIG_BLINK=n
+ * for example, to prj.conf. See Kconfig for the options or run
+ * west build -t menuconfig for an interacive configuration
+ * editor.
+ */
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h> 
+#include <zephyr/sys/__assert.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(oresat_mcxn947_solar, LOG_LEVEL_DBG);
+
+int main(void)
+{
+	printk("\n");
+	LOG_INF("Oresat MCXN947 Solar Board App\n");
+	return 0;
+}
