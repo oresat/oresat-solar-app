7c405c93794369df053cecfab1a087db1d9947a4
Pete Skeggs <plskeggs@noeticdesign.com>
2025-10-21 20:32:09 -0700

    debug I2C access

--- src/i2c_sensor.c
+++ src/i2c_sensor.c
@@ -25,96 +25,140 @@ LOG_MODULE_REGISTER(oresat_i2c_sensors, LOG_LEVEL_DBG);
 /* scheduling priority used by each thread */
 #define PRIORITY 7
 
-/* 1000 msec = 1 sec */
-#define BME_SLEEP_TIME_MS   5000
+static const struct device *const ina = DEVICE_DT_GET_ONE(ti_ina226);
+static const struct device *tmp1;
+static const struct device *tmp2;
 
-static const struct device *check_bme280_device(const struct device *bme_dev)
+static int init_ina226(void)
 {
-	if (bme_dev == NULL) {
-		/* No such node, or the node does not have status "okay". */
-		LOG_ERR("\nError: no device found.");
-		return NULL;
-	}
+	LOG_INF("Starting INA226 reading");
 
-	if (!device_is_ready(bme_dev)) {
-		LOG_ERR("\nError: Device \"%s\" is not ready; "
-		       "check the driver initialization logs for errors.",
-		       bme_dev->name);
-		return NULL;
+	if (!device_is_ready(ina)) {
+		LOG_ERR("Device %s is not ready.", ina->name);
+		return -ENODEV;
 	}
-
-	LOG_INF("Found device \"%s\", getting sensor data", bme_dev->name);
-	return bme_dev;
+	return 0;
 }
 
-/*
- * Get a device structure from a devicetree node with compatible
- * "bosch,bme280". (If there are multiple, just pick one.)
- */
-SENSOR_DT_READ_IODEV(iodev, DT_COMPAT_GET_ANY_STATUS_OKAY(bosch_bme280),
-		{SENSOR_CHAN_AMBIENT_TEMP, 0},
-		{SENSOR_CHAN_HUMIDITY, 0},
-		{SENSOR_CHAN_PRESS, 0});
-
-RTIO_DEFINE(ctx, 1, 1);
-
-static int handle_bme280(void)
+static int handle_ina226(void)
 {
-	int ret;
-	uint8_t buf[128];
-	const struct device *const bme_dev = DEVICE_DT_GET_ANY(bosch_bme280);
+	struct sensor_value v_bus;
+	struct sensor_value power;
+	struct sensor_value current;
+	int rc;
 
-	LOG_INF("Starting I2C sensor demo");
+	rc = sensor_sample_fetch(ina);
+	if (rc) {
+		LOG_ERR("Could not fetch sensor data: %d", rc);
+	} else {
+		sensor_channel_get(ina, SENSOR_CHAN_VOLTAGE, &v_bus);
+		sensor_channel_get(ina, SENSOR_CHAN_POWER, &power);
+		sensor_channel_get(ina, SENSOR_CHAN_CURRENT, &current);
 
-	/* Can we use the I2C sensor? */
-	if (check_bme280_device(bme_dev) == NULL) {
-		LOG_ERR("Could not find the BME280");
-		return -1;
+		printk("Bus: %f [V] -- "
+			   "Power: %f [W] -- "
+			   "Current: %f [A]\n",
+			   sensor_value_to_double(&v_bus),
+			   sensor_value_to_double(&power),
+			   sensor_value_to_double(&current));
 	}
-
-	/* Thread loop */
-	while (1) {
-		ret = sensor_read(&iodev, &ctx, buf, 128);
-		if (ret != 0) {
-			LOG_ERR("%s: sensor_read() failed: %d", bme_dev->name, ret);
-			break;
-		}
-
-		const struct sensor_decoder_api *decoder;
-
-		ret = sensor_get_decoder(bme_dev, &decoder);
-		if (ret != 0) {
-			LOG_ERR("%s: sensor_get_decode() failed: %d", bme_dev->name, ret);
-			break;
-		}
-
-		uint32_t temp_fit = 0;
-		struct sensor_q31_data temp_data = {0};
-
-		decoder->decode(buf, (struct sensor_chan_spec) {SENSOR_CHAN_AMBIENT_TEMP, 0},
-						&temp_fit, 1, &temp_data);
-
-		uint32_t press_fit = 0;
-		struct sensor_q31_data press_data = {0};
-
-		decoder->decode(buf, (struct sensor_chan_spec) {SENSOR_CHAN_PRESS, 0},
-						&press_fit, 1, &press_data);
-
-		uint32_t hum_fit = 0;
-		struct sensor_q31_data hum_data = {0};
-
-		decoder->decode(buf, (struct sensor_chan_spec) {SENSOR_CHAN_HUMIDITY, 0},
-						&hum_fit, 1, &hum_data);
-
-		printk("temp: %s%d.%d; press: %s%d.%d; humidity: %s%d.%d\n",
-			PRIq_arg(temp_data.readings[0].temperature, 6, temp_data.shift),
-			PRIq_arg(press_data.readings[0].pressure, 6, press_data.shift),
-			PRIq_arg(hum_data.readings[0].humidity, 6, hum_data.shift));
-
-		k_msleep(BME_SLEEP_TIME_MS);
-	}
-	return ret;
+	return rc;
 }
 
-K_THREAD_DEFINE(bme280_id, STACKSIZE, handle_bme280, NULL, NULL, NULL, PRIORITY, 0, 0);
+static int init_tmp101(void)
+{
+	LOG_INF("Starting TMP108 reading");
+
+	tmp1 = DEVICE_DT_GET_ONE(ti_tmp108);
+	tmp2 = tmp1; // DEVICE_NAME_GET(tmp101_cell2);
+
+	if (!tmp1 || !tmp2) {
+		LOG_ERR("Tmp108 devices not found");
+		return -ENODEV;
+	}
+
+	if (!device_is_ready(tmp1) || !device_is_ready(tmp2)) {
+		LOG_ERR("tmp108 devices not ready");
+		return -ENODEV;
+	}
+
+#if 0
+	sensor_attr_set(tmp1,
+			SENSOR_CHAN_AMBIENT_TEMP,
+			SENSOR_ATTR_TMP108_CONTINUOUS_CONVERSION_MODE,
+			NULL);
+
+#if CONFIG_APP_ENABLE_ONE_SHOT
+	enable_one_shot(tmp1);
+#endif
+
+#if CONFIG_APP_REPORT_TEMP_ALERTS
+	enable_temp_alerts(tmp1);
+#endif
+#endif
+
+	return 0;
+}
+
+int get_temperature_continuous(const struct device *tmp108)
+{
+
+	struct sensor_value temp_value;
+	int rc;
+
+	rc = sensor_channel_get(tmp108,
+				    SENSOR_CHAN_AMBIENT_TEMP,
+				    &temp_value);
+
+	if (rc) {
+		LOG_ERR("Sensor_channel_get failed: %d", rc);
+		return rc;
+	}
+
+	printk("temperature is %gC\n", sensor_value_to_double(&temp_value));
+	return 0;
+}
+
+static int handle_tmp101(void)
+{
+	int rc;
+
+	rc = sensor_sample_fetch(tmp1);
+	if (rc) {
+		LOG_ERR("tmp1 sensor_sample_fetch failed: %d", rc);
+		return rc;
+	}
+	//rc = sensor_sample_fetch(tmp2);
+	//if (rc) {
+	//	LOG_ERR("tmp2 sensor_sample_fetch failed: %d", rc);
+	//	break;
+	//}
+
+#if 1 // !CONFIG_APP_ENABLE_ONE_SHOT
+	get_temperature_continuous(tmp1);
+	//get_temperature_continuous(tmp2);
+#endif
+	return 0;
+}
+
+static int handle_i2c_sensors(void)
+{
+	int rc1;
+	int rc2;
+
+	rc1 = init_ina226();
+	rc2 = init_tmp101();
+	while (true) {
+		if (!rc1) {
+			rc1 = handle_ina226();
+		}
+		if (!rc2) {
+			rc2 = handle_tmp101();
+		}
+		k_sleep(K_MSEC(2000));
+	}
+	return 0;
+}
+
+K_THREAD_DEFINE(ina226_id, STACKSIZE, handle_i2c_sensors, NULL, NULL, NULL, PRIORITY, 0, 0);
 
