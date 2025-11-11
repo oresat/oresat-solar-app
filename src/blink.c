0d0acdab82847391132054279723df395aaa98fa
hx3d <slugtrailing@gmail.com>
2025-11-10 17:34:29 -0800

    initial port of bare bones solar thread without corner cutting

--- src/blink.c
+++ src/blink.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h> 
+#include <zephyr/sys/printk.h>
 #include <zephyr/sys/__assert.h>
 
 #include <zephyr/device.h>
